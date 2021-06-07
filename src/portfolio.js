9f92d40c7a1d173262972e5197fed6e4fb2581c3
Bartosz <bartptak@gmail.com>
2021-06-07 20:38:53 +0200

    Update

--- src/portfolio.js
+++ src/portfolio.js
@@ -11,8 +11,8 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Baerosz",
+  username: "Bartosz Ptak",
+  title: "Hi all, I'm Bartosz",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
